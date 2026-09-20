2704fa5f4699b16837cacdf4d24065e04d515bee
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-09-20 14:31:36 -0700

    Added utility lib

---
+++ project-2/lib/utils/utils.cpp
@@ -0,0 +1,46 @@
+#include "utils.hpp"
+#include "parser.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <string>
+
+std::optional<std::vector<Complex>>
+utils::read_file(std::string_view file_path,
+                 std::optional<std::size_t> read_until) {
+  std::ifstream file{file_path.data()};
+
+  if (!file.is_open()) {
+    std::cerr << "File not found: " << file_path << "\n";
+    return std::nullopt;
+  }
+
+  std::vector<Complex> output;
+  std::size_t lines_read{0};
+
+  while (!file.eof()) {
+    std::string buffer{};
+    std::getline(file, buffer);
+
+    const std::optional<Complex> number_opt{parse_cartesian(buffer)};
+    if (!number_opt) {
+      std::cerr << "Invalid Number: " << buffer << "\n";
+      return std::nullopt;
+    }
+
+    output.push_back(number_opt.value());
+    lines_read++;
+
+    if (read_until && lines_read == *read_until) {
+      break;
+    }
+  }
+
+  if (read_until && output.size() != *read_until) {
+    std::cerr << "Not enough numbers: " << *read_until << "\n";
+    return std::nullopt;
+  }
+
+  return output;
+}
